af78d0f0fc9624c47c681691bc959045fa0157ad
SHIBATA, Manabu <manabu.shibata@gmail.com>
2024-05-12 22:16:51 +0100

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+const autoprefixer = require("autoprefixer");
+
+/**
+ * @type {import{'postcss'}.ProcessOptions}
+ */
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {}
+    }
+}
